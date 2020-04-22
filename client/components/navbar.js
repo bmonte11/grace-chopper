b9fde15d3d83470383723b425f8b9876a2e4bcd4
Jason Li <iverefiner@gmail.com>
2020-04-22 11:43:29 -0500

    added logged in conditional rendering to navbar component

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,58 +4,77 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
-const Navbar = ({}) => {
+const Navbar = ({isLoggedIn}) => {
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          <i className="fas fa-utensils" />
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+    <React.Fragment>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <i className="fas fa-utensils" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">
-                Home <span className="sr-only">(current)</span>
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Products
-              </a>
-            </li>
-          </ul>
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Account
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Orders
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Cart
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Products
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav mr-auto">
+              {isLoggedIn ? (
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Account
+                    </a>
+                  </li>
+                </ul>
+              ) : (
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Login
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Sign Up
+                    </a>
+                  </li>
+                </ul>
+              )}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Orders
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Cart
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </React.Fragment>
   )
 }
 
