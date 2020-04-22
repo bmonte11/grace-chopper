375276d65d927a12f448820290a6867270868c7e
Jason Li <iverefiner@gmail.com>
2020-04-22 11:19:20 -0500

    added sampleSeed to script in json file

--- script/sample.js
+++ script/sample.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const db = require('../server/db')
 const {User, Order, Product, Item, Review} = require('../server/db/models')
 const faker = require('faker')
@@ -14,11 +16,11 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-const getRandomPrice = (min, max) => {
-  min = Math.ceil(min * 100)
-  max = Math.floor(max * 100)
-  return Math.floor(Math.random() * (max - min + 1)) / 100
-}
+// const getRandomPrice = (min, max) => {
+//   min = Math.ceil(min * 100)
+//   max = Math.floor(max * 100)
+//   return Math.floor(Math.random() * (max - min + 1)) / 100
+// }
 
 const pickRandomCategory = () => {
   const num = getRandomInt(1, 4)
@@ -115,4 +117,49 @@ makeFakeUsers(10)
 // console.log(fakeProducts);
 // console.log(fakeItems);
 // console.log(fakeReviews);
-console.log(fakeUsers)
+// console.log(fakeUsers)
+
+async function sampleSeed() {
+  await db.sync({force: true})
+  console.log('db synced!')
+
+  const orders = await Order.bulkCreate(fakeOrders)
+  const products = await Product.bulkCreate(fakeProducts)
+  const items = await Item.bulkCreate(fakeItems)
+  const reviews = await Review.bulkCreate(fakeReviews)
+  const users = await User.bulkCreate(fakeUsers)
+
+  console.log(`seeded ${orders.length} orders`)
+  console.log(`seeded ${products.length} products`)
+  console.log(`seeded ${items.length} items`)
+  console.log(`seeded ${reviews.length} reviews`)
+  console.log(`seeded ${users.length} users`)
+  console.log(`seeded successfully`)
+}
+
+// We've separated the `seed` function from the `runSeed` function.
+// This way we can isolate the error handling and exit trapping.
+// The `seed` function is concerned only with modifying the database.
+async function runSampleSeed() {
+  console.log('seeding...')
+  try {
+    await sampleSeed()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+// Execute the `seed` function, IF we ran this module directly (`node seed`).
+// `Async` functions always return a promise, so we can use `catch` to handle
+// any errors that might occur inside of `seed`.
+if (module === require.main) {
+  runSampleSeed()
+}
+
+// we export the seed function for testing purposes (see `./seed.spec.js`)
+module.exports = sampleSeed
