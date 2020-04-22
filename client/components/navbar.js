59b0589a43f38b4ee11a1e91b570bc6cfb1882bd
Jason Li <iverefiner@gmail.com>
2020-04-22 14:12:47 -0500

    deleted boilerplate navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -78,33 +78,6 @@ const Navbar = ({isLoggedIn}) => {
   )
 }
 
-// const Navbar = ({handleClick, isLoggedIn}) => (
-//   <div>
-//     <h1>BOILERMAKER</h1>
-//     <nav>
-//       {isLoggedIn ? (
-//         <div>
-//           {/* The navbar will show these links after you log in */}
-//           <Link to="/home">Home</Link>
-//           <a href="#" onClick={handleClick}>
-//             Logout
-//           </a>
-//         </div>
-//       ) : (
-//         <div>
-//           {/* The navbar will show these links before you log in */}
-//           <Link to="/login">Login</Link>
-//           <Link to="/signup">Sign Up</Link>
-//         </div>
-//       )}
-//     </nav>
-//     <hr />
-//   </div>
-// )
-
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
