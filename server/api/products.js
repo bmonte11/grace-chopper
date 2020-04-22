a4d6958ec3e06bc5c56475e78c4094f92c263d9d
Kevin Fergus <kevinfergus89@gmail.com>
2020-04-22 15:45:34 -0500

    added post, findbypk, and delete for product api routes

--- server/api/products.js
+++ server/api/products.js
@@ -3,12 +3,47 @@ const {Product} = require('../db/models')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
-  console.log('hitting the products route')
   try {
     const products = await Product.findAll({})
-    console.log(products)
     res.json(products)
   } catch (err) {
     next(err)
+  }
+})
+
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    product.update(req.body)
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const product = await Product.create(req.body)
+    res.json(product)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId)
+    await product.destroy()
+    res.sendStatus(204)
+  } catch (error) {
+    next(error)
   }
 })
