5b22a37e07e76387428ab1d147935adb9e50c88d
Jonah Ullman <jonahmullman@gmail.com>
2020-04-22 18:57:37 -0500

    Built single review component, edited reviews route to eager load user info, integrated with redux, edited single product component to render review list

--- client/components/SingleReview.js
+++ client/components/SingleReview.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
 const SingleReview = props => {
-  return <div>This is a SingleReview</div>
+  const review = props.review
+  return (
+    <div className="review">
+      <h4>{`${review.title} - ${review.rating}/5 Stars`}</h4>
+      <div>{`Reviewed by ${review.user.firstName} ${review.user.lastName}`}</div>
+      <p>{review.content}</p>
+    </div>
+  )
 }
+
+export default SingleReview
