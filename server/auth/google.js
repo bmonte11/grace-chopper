8d35df8ea163908fef9b42898c67d76c8df2000c
Jason Li <iverefiner@gmail.com>
2020-04-23 22:24:42 -0500

    changed log in redirect to /

--- server/auth/google.js
+++ server/auth/google.js
@@ -66,7 +66,7 @@ if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET) {
   router.get(
     '/callback',
     passport.authenticate('google', {
-      successRedirect: '/account',
+      successRedirect: '/',
       failureRedirect: '/login'
     })
   )
