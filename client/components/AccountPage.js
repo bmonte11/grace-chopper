5f821d19e3058be81382e14883759dba3a2b4f25
Jonah Ullman <jonahmullman@gmail.com>
2020-04-26 21:10:58 -0500

    Tweaked my account admin rendering

--- client/components/AccountPage.js
+++ client/components/AccountPage.js
@@ -1,16 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {NavLink} from 'react-router-dom'
 import {AdminDashboard} from '.'
 
 const AccountPage = props => {
   return (
     <div id="account-page">
       <h2>My Account</h2>
-      <div>
-        Everything below here will either be rendered conditionally, or a link
-        to an admin dashboard will be rendered conditionally. Dunno yet.
-      </div>
-      <AdminDashboard />
+      <div>Below link will be conditionally rendered laaaaater</div>
+      <NavLink to="/account/admin">Admin Dashboard</NavLink>
     </div>
   )
 }
